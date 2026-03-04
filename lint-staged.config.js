3a242a2b1bcae208bb30659b53c3248d922a9b3b
Manh Vu <manhvu337@gmail.com>
2026-03-05 00:13:00 +0700

    feature showtime/service

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,15 +1,9 @@
-{
-  "scripts": {
-    "lint": "eslint .",
-      "format": "prettier --write \"src/**/*.{js,jsx,ts,tsx,css,md}\""
-  },
-  "lint-staged": {
-    "src/**/*.{js,jsx,ts,tsx}": [
-      "eslint --fix",
-      "prettier --write"
-    ],
-      "src/**/*.{css,md,json}": [
-        "prettier --write"
-      ]
-  }
-}
+export default {
+  "src/**/*.{js,jsx,ts,tsx}": [
+    "npx eslint --fix",
+    "prettier --write"
+  ],
+  "src/**/*.{css,md,json}": [
+    "prettier --write"
+  ]
+};
